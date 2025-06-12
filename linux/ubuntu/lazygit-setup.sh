4e8c8517a879d9941a82bf82c9dd3fdcb4f0e9ad
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-06-12 23:42:34 +0200

    feat(linux): Added official lazygit installation setup.

---
+++ linux/ubuntu/lazygit-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ----------------------------------------------------------------------------
+# Script: lazygit-setup.sh
+# Purpose: Installs lazygit for neovim plugin
+# Usage: ./lazygit-setup.sh
+# ----------------------------------------------------------------------------
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+
+sudo install lazygit -D -t /usr/local/bin/
+
+# Cleanup
+rm -rf lazygit.tar.gz
+rm -rf tar xf lazygit.tar.gz lazygit
