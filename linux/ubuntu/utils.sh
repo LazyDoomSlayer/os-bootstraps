893a3fd25352114bab96e540b70bd2de7a83ecd5
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-06-13 17:18:57 +0200

    feat(linux): Add snap installer packages helper.

--- linux/ubuntu/utils.sh
+++ linux/ubuntu/utils.sh
@@ -1,28 +1,51 @@
 #!/bin/bash
 
-is_installed() {
+is_installed_apt() {
   dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -q "install ok installed"
 }
 
-install_packages() {
+is_installed_snap() {
+  snap list "$1" &>/dev/null
+}
+
+install_apt_packages() {
   local packages=("$@")
   local to_install=()
 
-  # Missing packages
   for pkg in "${packages[@]}"; do
-    if ! is_installed "$pkg"; then
+    if ! is_installed_apt "$pkg"; then
       to_install+=("$pkg")
     fi
   done
 
-  # Update Once, Install others
-  if [ ${#to_install[@]} -ne 0 ]; then
-    echo "Updating package lists…"
+  if [ ${#APT_QUEUE[@]} -gt 0 ]; then
+    echo "Updating apt package lists..."
     sudo apt-get update -qq
-
-    echo "Installing: ${to_install[*]}"
-    sudo apt-get install -y "${to_install[@]}"
+    echo "Installing APT packages: ${APT_QUEUE[*]}"
+    sudo apt-get install -y "${APT_QUEUE[@]}"
   else
-    echo "All requested packages are already installed."
+    echo "No APT packages to install."
+  fi
+}
+
+install_snap_packages() {
+  local pkgs=("$@")
+  local to_install=()
+
+  for pkg in "${pkgs[@]}"; do
+    if ! is_installed_snap "$pkg"; then
+      to_install+=("$pkg")
+    fi
+  done
+
+  if [ ${#to_install[@]} -gt 0 ]; then
+    for snap_pkg in "${to_install[@]}"; do
+      echo "Installing snap package: $snap_pkg"
+      if ! sudo snap install --classic "$snap_pkg"; then
+        sudo snap install "$snap_pkg"
+      fi
+    done
+  else
+    echo "All Snap packages are already installed: ${pkgs[*]}"
   fi
 }
